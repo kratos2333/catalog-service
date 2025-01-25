2b6498adbea5a37daaf20260c02dac734ab39a32
kevin <kevinjia1984@gmail.com>
2025-01-26 10:52:19 +1100

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# first project in cloud native spring in action
+
+https://github.com/ThomasVitale/cloud-native-spring-in-action
---
+++ src/main/java/com/kevin/catalogservice/CatalogServiceApplication.java
@@ -0,0 +1,18 @@
+package com.kevin.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// TODO: note: use buildPack to build the app into image:
+// ./mvnw spring-boot:build-image -DskipTests
+// the built image will be named : catalog-service:0.0.1-SNAPSHOT by default (<project_name>:)
+// $ docker run --rm --platform linux/amd64 --name catalog-service -p 8080:8080 catalog-service:0.0.1-SNAPSHOT
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+    }
+
+}
